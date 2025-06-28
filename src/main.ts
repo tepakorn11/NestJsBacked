1ff2ba1b040164179c016f04ddbff841f1480f81
Tepakorn <tepakornkam@gmail.com>
2025-06-28 22:40:20 +0700

    deploy 2

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const app = await NestFactory.create(AppModule, {
+  logger: ['log', 'error', 'warn', 'debug'], // ✅ เพิ่ม logger debug
+});
+
 
   app.enableCors({
     origin: "http://localhost:3000",
